e4af2610123f744a7e6b12ac12f5c541bdbf6101
dev-ptera <dev.ptera@gmail.com>
2022-02-20 12:20:04 -0500

    Only show settings when having secret

--- src/app/components/account-settings/account-settings.component.ts
+++ src/app/components/account-settings/account-settings.component.ts
@@ -5,7 +5,7 @@ import { SeedService } from '@app/services/seed.service';
 @Component({
     selector: 'app-account-settings',
     template: `
-        <blui-user-menu class="app-theme-picker" menuTitle="Account" [(open)]="userMenuOpen">
+        <blui-user-menu class="app-theme-picker" menuTitle="Account" [(open)]="userMenuOpen" *ngIf="show()">
             <mat-icon blui-avatar>settings</mat-icon>
             <mat-nav-list blui-menu-body [style.paddingTop.px]="0">
                 <blui-info-list-item [dense]="true" (click)="clearData()">
@@ -27,5 +27,9 @@ export class AppAccountSettingsComponent {
         this._seedService.clearSeed();
         void this._router.navigate(['']);
         this.userMenuOpen = false;
+    }
+
+    show(): boolean {
+        return this._seedService.hasSecret();
     }
 }
