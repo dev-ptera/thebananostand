f387d0f2c75131e1c6cc254ee6efbb3a6248e0cc
dev-ptera <dev.ptera@gmail.com>
2022-08-10 15:28:31 -0400

    Add e2e test

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    chromeWebSecurity: false,
+
+      setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
