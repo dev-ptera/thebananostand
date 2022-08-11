5153e58cf6b9e2e68432ead733039b9601cda7a1
dev-ptera <dev.ptera@gmail.com>
2022-08-11 08:21:05 -0400

    Try again

--- cypress/e2e/user_session.cy.js
+++ cypress/e2e/user_session.cy.js
@@ -3,22 +3,22 @@ describe("User Session", () => {
     const LOW_FUND_SEED = '727A5E960F6189BBF196D84A6B7715D0A78DE82AC15BBDB340540076768CDB31';
     const root = 'http://localhost:4200'
 
-    beforeEach(() => {
+    const reload = () => {
         cy.intercept(root).as('home');
         cy.visit(root);
         cy.wait('@home'); // once the route resolves, cy.wait will resolve as well
-    });
-
-    const reload = () => {
-        cy.reload();
     }
+
+    beforeEach(() => {
+        cy.clearLocalStorage();
+        reload();
+    });
 
     it("should login with just a seed (no password)", () => {
         cy.get('#enter-secret').click();
         cy.get('#secret-input').type(LOW_FUND_SEED);
         cy.get('#secret-next').click();
         cy.get('#secret-next').click();
-        cy.wait(5000);
         reload();
         cy.window().then(() => {
 
@@ -36,7 +36,6 @@ describe("User Session", () => {
         cy.get('#secret-next').click();
         cy.get('#password-input').type(password);
         cy.get('#secret-next').click();
-        cy.wait(5000);
         reload();
         cy.get('#login-wrapper');
         cy.get('#active-wallet-password-input').type(password);
@@ -52,7 +51,6 @@ describe("User Session", () => {
         cy.get('#secret-next').click();
         cy.get('#password-input').type(password);
         cy.get('#secret-next').click();
-        cy.wait(5000);
         reload();
         cy.get('#login-wrapper');
         cy.get('#active-wallet-password-input').type(incorrectPassword);
@@ -67,12 +65,19 @@ describe("User Session", () => {
         cy.get('#secret-next').click();
         cy.get('#password-input').type(password);
         cy.get('#secret-next').click().then(() => {
-            cy.wait(5000);
-            expect(localStorage.getItem('bananostand_encryptedSeed')).to.be.ok;
+            cy.window().then(
+                (window) => {
+                    expect(window.localStorage.getItem('bananostand_encryptedSeed')).to.be.ok;
+                }
+            );
         })
         cy.get('#account-settings').click();
         cy.get('#clear-data-button').click().then(() => {
-            expect(localStorage.getItem('bananostand_encryptedSeed')).to.not.be.ok;
+            cy.window().then(
+                (window) => {
+                    expect(window.localStorage.getItem('bananostand_encryptedSeed')).to.not.be.ok;
+                }
+            );
         })
     });
 });
