854ba7c0b1a8a9f78e834980399f0191982067cc
dev-ptera <dev.ptera@gmail.com>
2022-08-10 21:42:01 -0400

    Update CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:4200',
         chromeWebSecurity: false,
-
+        video: false,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
