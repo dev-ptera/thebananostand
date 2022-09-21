9d1174620483e0b687518258af183fcad8e9510b
dev-ptera <dev.ptera@gmail.com>
2022-09-20 20:01:29 -0400

    Lint project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,8 @@ export class AppComponent {
     }
 
     showBanana(): boolean {
-        const unlocked = this._secretService.isLocalSecretUnlocked() || this._secretService.isLocalLedgerUnlocked();
-        const isBigScreen = !this._vp.isSmall() && !this._vp.isMedium();
+        // const unlocked = this._secretService.isLocalSecretUnlocked() || this._secretService.isLocalLedgerUnlocked();
+        // const isBigScreen = !this._vp.isSmall() && !this._vp.isMedium();
         //  return !unlocked && isBigScreen;
         return false;
     }
