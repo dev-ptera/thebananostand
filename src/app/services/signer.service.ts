e2a165473c6a63396a8e9d919add9b5e4bb85358
dev-ptera <dev.ptera@gmail.com>
2023-03-20 19:38:36 -0400

    Try swap transport

--- src/app/services/signer.service.ts
+++ src/app/services/signer.service.ts
@@ -1,6 +1,12 @@
 import { AppStateService } from '@app/services/app-state.service';
 import { SecretService } from '@app/services/secret.service';
 import { Injectable } from '@angular/core';
+import { Banano } from 'hw-app-nano';
+
+import TransportU2F from '@ledgerhq/hw-transport-u2f';
+import TransportUSB from '@ledgerhq/hw-transport-webusb';
+import TransportHID from '@ledgerhq/hw-transport-webhid';
+import Transport from '@ledgerhq/hw-transport';
 
 export type BananoifiedWindow = {
     bananocoin: any;
@@ -12,11 +18,33 @@ export type BananoifiedWindow = {
 
 declare let window: BananoifiedWindow;
 
+export interface LedgerData {
+    banano: any|null;
+    transport: Transport|null;
+}
+
+
 @Injectable({
     providedIn: 'root',
 })
 export class SignerService {
-    constructor(private readonly _secretService: SecretService, private readonly _appStateService: AppStateService) {}
+
+    DynamicTransport: any;
+    supportsU2F = false;
+    supportsWebHID = false;
+    supportsWebUSB = false;
+    supportsBluetooth = false;
+    supportsUSB = false;
+
+    ledger: LedgerData = {
+        banano: null,
+        transport: null,
+    };
+
+    constructor(private readonly _secretService: SecretService, private readonly _appStateService: AppStateService) {
+        this.doStuff();
+    }
+
 
     /** Checks if ledger is connected via USB & is unlocked, ready to use. */
     async checkLedgerOrError(): Promise<void> {
@@ -51,9 +79,41 @@ export class SignerService {
             const account = window.bananocoinBananojs.getBananoAccount(publicKey);
             return account;
         }
-        const accountData = await window.bananocoin.bananojsHw.getLedgerAccountData(accountIndex);
-        const account = accountData.account;
-        return account;
+
+     //   const accountData = await window.bananocoin.bananojsHw.getLedgerAccountData(accountIndex);
+    //    const account = accountData.account;
+       // return account;
+
+        return await this.getLedgerAccountWeb(accountIndex);
+    }
+
+
+
+    async doStuff(): Promise<any> {
+        await this.checkBrowserSupport();
+        await this.detectUsbTransport();
+        const appConfig = await this.ledger.banano.getAppConfiguration();
+        console.log(appConfig);
+        const address = await this.getLedgerAccountWeb(0);
+        console.log(address);
+    }
+
+    async checkBrowserSupport() {
+        await Promise.all([
+            TransportHID.isSupported().then(supported => this.supportsWebHID = supported),
+            TransportUSB.isSupported().then(supported => this.supportsWebUSB = supported),
+        ]);
+    }
+
+    async detectUsbTransport(): Promise<void> {
+        if (this.supportsWebUSB) {
+            await this.loadTransport(TransportUSB);
+        } else if (this.supportsWebHID) {
+            await this.loadTransport(TransportHID);
+        } else {
+            // Legacy browsers
+            await this.loadTransport(TransportU2F);
+        }
     }
 
     async getAccountSigner(index: number): Promise<string> {
@@ -62,5 +122,34 @@ export class SignerService {
             return await window.bananocoinBananojs.getPrivateKey(seed, index);
         }
         return await window.bananocoin.bananojsHw.getLedgerAccountSigner(index);
+    }
+
+    async getLedgerAccountWeb(accountIndex: number) {
+        try {
+            return await this.ledger.banano.getAddress(this.ledgerPath(accountIndex));
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    ledgerPath(accountIndex: number) {
+        const walletPrefix = `44'/198'/`;
+        return `${walletPrefix}${accountIndex}'`;
+    }
+
+    async getLedgerAccount(accountIndex: number) {
+        return await this.getLedgerAccountWeb(accountIndex);
+    }
+
+    async loadTransport(transport: typeof TransportU2F): Promise<void> {
+        return new Promise((resolve, reject) => {
+            transport.create().then(trans => {
+                this.ledger.transport = trans;
+                const waitTimeout = 300000;
+                this.ledger.transport.setExchangeTimeout(waitTimeout); // 5 minutes
+                this.ledger.banano = new Banano(trans);
+                resolve();
+            }).catch(reject);
+        });
     }
 }
